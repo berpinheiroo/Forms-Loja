6253a9af05f7cb0c7c3f14138673e17d9686a42b
Bernardo Pinheiro <111240182+berpinheiroo@users.noreply.github.com>
2023-02-14 16:27:26 -0300

    Log

--- script.js
+++ script.js
@@ -4,24 +4,31 @@ const funcionamentoLoja = document.getElementById('funcionamento');
 
 nomeLoja.addEventListener('focus', (event) => {
     event.target.style.outlineColor = 'purple';
+    nomeLoja.value = window.localStorage.getItem('Nome da loja')
 });
 
 nomeLoja.addEventListener('blur', (event) => {
     event.target.style.borderColor = '';
+    window.localStorage.setItem("Nome da loja" , event.target.value)
 });
 
 enderecoLoja.addEventListener('focus', (event) => {
     event.target.style.outlineColor = 'purple';
+    enderecoLoja.value = window.localStorage.getItem('Endereco da loja')
 });
 
 enderecoLoja.addEventListener('blur', (event) => {
     event.target.style.borderColor = '';
+    window.localStorage.setItem("Endereco da loja" , event.target.value)
 });
 
 funcionamentoLoja.addEventListener('focus', (event) => {
     event.target.style.outlineColor = 'purple';
+    funcionamentoLoja.value = window.localStorage.getItem('Funcionamento da loja')
 });
 
 funcionamentoLoja.addEventListener('blur', (event) => {
     event.target.style.borderColor = '';
+    window.localStorage.setItem("Funcionamento da loja" , event.target.value)
 });
+
